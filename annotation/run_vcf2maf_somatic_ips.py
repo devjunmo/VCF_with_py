bbeaa3f56d53ff61fcdb5d9a5923d4c0c6cc035c
yajumo93 <yajumo93@gmail.com>
2021-09-06 01:49:15 +0000

    .

--- annotation/run_vcf2maf_somatic_ips.py
+++ annotation/run_vcf2maf_somatic_ips.py
@@ -3,40 +3,41 @@ from glob import glob
 import subprocess as sp
 
 
-# input_dir = r'/data_244/VCF/gatherd_WGS_WES/WES_specific/'
-# input_dir = r'/data_244/VCF/gatherd_WGS_WES/common/'
-# input_dir = r'/data_244/VCF/gatherd_WGS_WES/WGS_specific/'
-# input_dir = r'/data_244/WGS/HN00146173/gs/hardfiltered/'
-# input_dir = r'/data_244/VCF/gatherd_WGS_WES_interval_apply/WES_specific/'
-# input_dir = r'/data_244/VCF/gatherd_WGS_WES_interval_apply/WGS_specific/'
-# input_dir = r'/data_244/VCF/gatherd_noDP_WGS_WES_interval_apply/WGS_specific/'
-# input_dir = r'/data_244/WGS/HN00146173/gs/hardfiltered/interval_apply_vcf/'
-# input_dir = r'/data_244/stemcell/WES/ips_recal_bam/tumor_only/filtered_vcf/pass_only/'
-# input_dir = r'/data_244/stemcell/WES/ips_recal_bam/tumor_only/filtered_vcf/inc_germ/'
+
+
 # input_dir = r'/data_244/stemcell/WES/ips_recal_bam/vardict_tumor_only/'
 # input_dir = r'/data_244/stemcell/WES/ips_recal_bam/vardict_tumor_only/passonly/'
 # input_dir = r'/data_244/stemcell/WES/hg38_pp/gs/merged/'
-input_dir = r'/data_244/stemcell/WES/hg38_pp/gs/merged/'
+# input_dir = r'/data_244/stemcell/WES/hg38_gdc_ips_tech/mutect2_tumor_only/'
+# input_dir = r'/data_244/stemcell/WES/hg38_gdc_ips_etc/gs/merged_VCF/'
+input_dir = r'/data_244/stemcell/WES/hg38_gdc_ips_etc/mutect2_tumor_only/'
 
 
-input_format = r'*.vcf'
+input_format = r'*_filtered.vcf'
 # input_format = r'hardFiltered_SNP_*'
-output_suffix = r'_germline'
+# input_format = r'*.vcf'
+
+# output_suffix = r'_HAP'
+output_suffix = r'_MUT2'
 
 output_dir_name = r'maf/'
 tmp_dir = input_dir + 'vep_vcf/'
 # fasta_path = r'/data_244/refGenome/b37/human_g1k_v37.fasta'
-fasta_path = r'/data_244/refGenome/hg38/v0/Homo_sapiens_assembly38.fasta'
+# fasta_path = r'/data_244/refGenome/hg38/v0/Homo_sapiens_assembly38.fasta'
+fasta_path = r'/data_244/refGenome/hg38/v0/gdc/GRCh38.d1.vd1.fa'
+
+# vep_data = r'/data_244/vep_data/homo_sapiens/102_GRCh38'
+ref_ver = 'GRCh38' # GRCh37 
 
 SRC_DIR = r"/home/pbsuser/mskcc-vcf2maf-754d68a/"
 SRC_PATH = SRC_DIR + "vcf2maf.pl"
 
 
 ## pbs config
-pbs_N = "ips_ant_germline"
-pbs_o = input_dir + "qsub_log/"
+pbs_N = "ips.mut.gdc.hg38"
+pbs_o = input_dir + "qsub_log_maf/"
 pbs_j = "oe"
-pbs_l_core = 2
+pbs_l_core = 4
 
 
 output_dir = input_dir + output_dir_name
@@ -69,5 +70,5 @@ for i in range(len(input_lst)):
     # sp.call(rf"perl vcf2maf.pl --input-vcf {input_vcf_path} --output-maf {output_maf_path} --ref-fasta {fasta_path} --tmp-dir {tmp_dir}", shell=True)
 
     sp.call(f'echo "perl {SRC_PATH} --input-vcf {input_vcf_path} --output-maf {output_maf_path} --ref-fasta {fasta_path} --tmp-dir {tmp_dir} \
-                --tumor-id {f_name} --vcf-tumor-id {f_name}" \
+                --tumor-id {f_name} --vcf-tumor-id {f_name} --ncbi-build {ref_ver}" \
               | qsub -N {pbs_N} -o {pbs_o} -j {pbs_j} -l select={pbs_l_core} &', shell=True)
